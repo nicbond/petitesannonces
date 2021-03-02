b94663f89a5cbfa56eff0b93d17bbb104fe8c8d1
nicbond <nicolasmartins78650@free.fr>
2021-03-02 11:01:58 +0100

    Add firstname and lastname property to Users entity

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -48,6 +48,16 @@ class Users implements UserInterface
      */
     private $annonces;
 
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $firstname;
+
     public function __construct()
     {
         $this->annonces = new ArrayCollection();
@@ -169,6 +179,30 @@ class Users implements UserInterface
                 $annonce->setUsers(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
 
         return $this;
     }
