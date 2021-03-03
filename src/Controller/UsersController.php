81e6e17990a620308bce243ba691d78bda895f94
nicbond <nicolasmartins78650@free.fr>
2021-03-03 10:48:20 +0100

    Make an user profil page

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -28,9 +28,7 @@ class UsersController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('users/index.html.twig', [
-            'controller_name' => 'UsersController',
-        ]);
+        return $this->render('users/index.html.twig');
     }
 
     /**
